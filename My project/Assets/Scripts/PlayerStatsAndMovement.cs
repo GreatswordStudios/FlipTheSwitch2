cbe160946e0767ad91d771b2794f82ea15df5165
BrianStrigel <greatswordstudios@gmail.com>
2024-10-05 14:56:27 -0400

    updates to player movement

---
+++ My project/Assets/Scripts/PlayerStatsAndMovement.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 m_Position = Vector2.zero;
+    float speed = 5f;
+    bool isActive = true;
+    string name = "";
+    int maxHealth = 4;
+    int currentHealth = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+
+                Vector2 movement = new Vector2(0, verticalInput);
+                movement.Normalize();
+                transform.Translate(movement * speed * Time.deltaTime);
+
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                Vector2 movement = new Vector2(0, verticalInput);
+                movement.Normalize();
+                transform.Translate(movement * speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                Vector2 movement = new Vector2(horizontalInput, 0);
+                movement.Normalize();
+                transform.Translate(movement * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Vector2 movement = new Vector2(horizontalInput, 0);
+                movement.Normalize();
+                transform.Translate(movement * speed * Time.deltaTime);
+            }
+        }
+
+    }
+        
+}
